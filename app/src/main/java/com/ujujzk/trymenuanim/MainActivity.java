a463ccbe68971fbcd0faab742a193e284d64f2a7
Taran <ujujzk@gmail.com>
2017-03-30 15:21:07 +0300

    dropdown menu as separate class

--- app/src/main/java/com/ujujzk/trymenuanim/MainActivity.java
+++ app/src/main/java/com/ujujzk/trymenuanim/MainActivity.java
@@ -11,192 +11,42 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
+import android.widget.Toast;
 
 import com.ujujzk.trymenuanim.customView.DropdownMenu;
 import com.ujujzk.trymenuanim.customView.LambdableAnimation;
 import com.ujujzk.trymenuanim.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
+/**
+ * @author Rainbow Dash from Softensy Team
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-
-    boolean menuIsShown = true;
-
-    List<View> menuViews = new ArrayList<>();
-
+    DropdownMenu ddm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        menuViews.add(binding.view1);
-        menuViews.add(binding.view2);
-        menuViews.add(binding.view3);
-        menuViews.add(binding.view4);
-        menuViews.add(binding.view5);
-        setMenuViewsParams();
+        ddm = DropdownMenu.getNewBuilder(this)
+                .addItem("Item 1", R.color.menu_1, R.drawable.ic_cloud_upload_white_18dp)
+                .addItem("Item 2", R.color.menu_2, R.drawable.ic_help_white_18dp)
+                .addItem("Item 3", R.color.menu_3, R.drawable.ic_map_white_18dp)
+                .addItem("Item 4", R.color.menu_4, R.drawable.ic_person_white_18dp)
+                .addItem("Item 5", R.color.menu_5, R.drawable.ic_settings_white_18dp)
+                .addListener(itemName -> {
+                    ddm.hideMenu();
+                    Toast.makeText(this,itemName, Toast.LENGTH_SHORT).show();})
+                .build();
 
 
-        binding.startAnim.setOnClickListener(v -> animMenu());
+        binding.startAnim.setOnClickListener(v -> ddm.showMenu());
     }
-
-
-    void animMenu() {
-        if (menuIsShown) {
-            hideMenu();
-        } else {
-            showMenu();
-        }
-    }
-
-    private void hideMenu() {
-
-//        Collections.reverse(menuViews);
-        liftMenuItem(menuViews.size() - 1, menuViews);
-        menuIsShown = false;
-    }
-
-    private void liftMenuItem(final int position, final List<View> views) {
-
-        if (position >= 0) {
-
-            final View v = views.get(position);
-
-            Animation liftAnim = new TranslateAnimation(0, 0, 0, -v.getHeight());
-            liftAnim.setDuration(200);
-            liftAnim.setFillAfter(true);
-            v.startAnimation(liftAnim);
-            new Handler().postDelayed(
-                    () -> {
-                        int newPos = position - 1;
-                        liftMenuItem(newPos, views);
-                    },
-                    50
-            );
-        }
-    }
-
-    private void showMenu() {
-
-        dropMenuItem(0, menuViews);
-        menuIsShown = true;
-    }
-
-
-    private void dropMenuItem(final int position, final List<View> views) {
-
-        if (position < views.size()) {
-            final View v = views.get(position);
-            Animation shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake);
-            LambdableAnimation.with(shakeAnim).setOnAnimationListener(
-                    animation -> increaseView(v),
-                    animation -> decreaseView(v)
-            );
-            Animation dropDownAnim = new TranslateAnimation(0, 0, -v.getHeight(), 0);
-            dropDownAnim.setDuration(200);
-            dropDownAnim.setFillAfter(true);
-            LambdableAnimation.with(dropDownAnim).setOnAnimationListener(
-                    animation -> {
-                    },
-                    animation -> {
-                        v.startAnimation(shakeAnim);
-                        int newPos = position + 1;
-                        dropMenuItem(newPos, views);
-                    }
-            );
-            v.startAnimation(dropDownAnim);
-        }
-    }
-
-
-    private void increaseView(View v) {
-        //used only for shake animation
-        //makes view bigger to hide space between view and parent when view shakes
-        v.layout(
-                -(int) this.getResources().getDimension(R.dimen.quickplay_offset),
-                -(int) this.getResources().getDimension(R.dimen.quickplay_offset),
-                v.getWidth() + (int) this.getResources().getDimension(R.dimen.quickplay_offset),
-                v.getHeight());
-    }
-
-    private void decreaseView(View v) {
-        //used only for shake animation
-        //turns back view size after increaseView()
-        //delay needs because otherwise it is executed when animation hasn't stop yet
-        new Handler().postDelayed(
-                () -> v.layout(
-                        0,
-                        0,
-                        v.getWidth() - 2 * (int) this.getResources().getDimension(R.dimen.quickplay_offset),
-                        v.getHeight() - (int) this.getResources().getDimension(R.dimen.quickplay_offset)),
-                500
-        );
-    }
-
-    View.OnClickListener cl = v -> {animMenu();};
-
-    private void setMenuViewsParams() {
-        int n = menuViews.size();
-        int screenHeight = screenHeight();
-        int itemHeight = screenHeight / n;
-
-        Log.w("mytag", "screenHeight: " + screenHeight);
-        Log.w("mytag", "itemHeight: " + itemHeight);
-
-
-        int multiplaer = n;
-
-        ViewGroup.LayoutParams parentPar;
-        ViewGroup.LayoutParams childPar;
-
-        ViewGroup parent;
-        View child;
-
-        //it needs to avoid space between last menu element and screen bottom
-        parent = (ViewGroup) menuViews.get(0);
-        child = parent.getChildAt(0);
-        parent.setOnClickListener(cl);
-        childPar = child.getLayoutParams();
-        childPar.height = itemHeight;
-        child.setLayoutParams(childPar);
-        parentPar = parent.getLayoutParams();
-        parentPar.height = screenHeight;
-        parent.setLayoutParams(parentPar);
-
-
-        for (int i = 1; i < n; i++) {
-            parent = (ViewGroup) menuViews.get(i);
-            child = parent.getChildAt(0);
-            parent.setOnClickListener(cl);
-            childPar = child.getLayoutParams();
-            childPar.height = itemHeight;
-            child.setLayoutParams(childPar);
-            parentPar = parent.getLayoutParams();
-            parentPar.height = itemHeight*(--multiplaer);
-            parent.setLayoutParams(parentPar);
-        }
-
-    }
-
-    private int screenHeight() {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        return displayMetrics.heightPixels - getStatusBarHeight();
-    }
-
-    public int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
-
 
 }
--- app/src/main/java/com/ujujzk/trymenuanim/customView/DropdownMenu.java
+++ app/src/main/java/com/ujujzk/trymenuanim/customView/DropdownMenu.java
@@ -1,16 +1,26 @@
 package com.ujujzk.trymenuanim.customView;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ujujzk.trymenuanim.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Rainbow Dash on 29.03.2017.
+ * @author Rainbow Dash from Softensy Team
  */
 
 public class DropdownMenu {
@@ -19,81 +29,229 @@ public class DropdownMenu {
     private List<ItemClickListener> listeners;
     private ViewGroup rootView;
     private Activity activity;
-    private boolean menuIsShown;
+    private boolean isShown;
+    private int shakeExtraSpace;
 
     private DropdownMenu() {
         //to avoid use empty constructor
-        throw new AssertionError();
+        throw new AssertionError("Don't use empty constructor of DropdownMenu");
     }
 
-    DropdownMenu(List<View> menuElements, List<ItemClickListener> listeners, Activity activity, ViewGroup rootView) {
-        this.menuItems = menuElements;
-        this.listeners = listeners;
+    private DropdownMenu(Activity activity) {
         this.activity = activity;
-        this.rootView = rootView;
-        menuIsShown = false;
+        this.isShown = true;
+        this.shakeExtraSpace = (int) activity.getResources().getDimension(R.dimen.extra_space);
     }
 
     public void addListener(final ItemClickListener itemClickListener) {
         listeners.add(itemClickListener);
     }
 
-    void showMenu() {
-        //TODO
-        menuIsShown = true;
+    public boolean isShown() {
+        return isShown;
     }
 
-    void hideMenu() {
-        //TODO
-        menuIsShown = false;
+    public void hideMenu() {
+        liftMenuItem(menuItems.size() - 1, menuItems);
+        isShown = false;
     }
+
+    private void liftMenuItem(final int position, final List<View> views) {
+        int nexPosition = position - 1;
+        if (position >= 0) {
+            final View v = views.get(position);
+            v.setClickable(false); //without that the view remains clickable after hiding
+            Animation liftAnim = new TranslateAnimation(0, 0, 0, -v.getHeight());
+            liftAnim.setDuration(200);
+            liftAnim.setFillAfter(true);
+            v.startAnimation(liftAnim);
+            new Handler().postDelayed(
+                    () -> liftMenuItem(nexPosition, views),
+                    50
+            );
+        }
+    }
+
+    public void showMenu() {
+        dropMenuItem(0, menuItems);
+        isShown = true;
+    }
+
+    private void dropMenuItem(final int position, final List<View> views) {
+        int nextPosition = position + 1;
+        if (position < views.size()) {
+            final View v = views.get(position);
+            v.setClickable(true);
+            Animation shakeAnim = AnimationUtils.loadAnimation(activity, R.anim.shake);
+            LambdableAnimation.with(shakeAnim).setOnAnimationListener(
+                    animation -> increaseView(v, position),
+                    animation -> decreaseView(v, position)
+            );
+            Animation dropDownAnim = new TranslateAnimation(0, 0, -v.getHeight(), 0);
+            dropDownAnim.setDuration(200);
+            dropDownAnim.setFillAfter(true);
+            LambdableAnimation.with(dropDownAnim).setOnAnimationListener(
+                    animation -> {
+                    },
+                    animation -> {
+                        if (position == 0) {
+                            //when shakes whole last item it looks bad
+                            v.findViewById(R.id.item_name).startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake));
+                            v.findViewById(R.id.item_drawable).startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake));
+                        } else {
+                            v.startAnimation(shakeAnim);
+                        }
+                        dropMenuItem(nextPosition, views);
+                    }
+            );
+            v.startAnimation(dropDownAnim);
+        }
+    }
+
+
+    private void increaseView(View v, int position) {
+        //used only for shake animation
+        //makes view bigger to hide space between view and parent when view shakes
+        v.layout(
+                -shakeExtraSpace,
+                -shakeExtraSpace,
+                v.getWidth() + shakeExtraSpace,
+                v.getHeight());
+    }
+
+    private void decreaseView(View v, int position) {
+        //used only for shake animation
+        //turns back view size after increaseView()
+        //delay needs because otherwise it is executed when animation hasn't stop yet
+        new Handler().postDelayed(
+                () -> v.layout(0, 0,
+                        v.getWidth() - 2 * shakeExtraSpace,
+                        v.getHeight() - shakeExtraSpace),
+                500
+        );
+    }
+
 
     public static Builder getNewBuilder(Activity activity) {
         return new Builder(activity);
     }
 
 
-
     public static class Builder {
 
-        List<View> _menuItems = new ArrayList<>();
-        List<ItemClickListener> _listeners = new ArrayList<>();
-        ViewGroup _rootView;
-        Activity _activity;
+        DropdownMenu dropdownMenu;
+
+//        List<View> _menuItems = new ArrayList<>();
+//        List<ItemClickListener> _listeners = new ArrayList<>();
+//        ViewGroup _rootView;
+//        Activity _activity;
 
         private Builder(Activity activity) {
-            _activity = activity;
-            this._rootView = (ViewGroup) activity.findViewById(android.R.id.content);
+            dropdownMenu = new DropdownMenu(activity);
+            dropdownMenu.rootView = (ViewGroup) activity.findViewById(android.R.id.content);
+            dropdownMenu.listeners = new ArrayList<>();
+            dropdownMenu.menuItems = new ArrayList<>();
         }
 
+        private Builder() {
+            //to avoid use empty constructor
+            throw new AssertionError("Don't use empty constructor of DropdownMenu#Builder ");
+        }
 
         public Builder addListener(final ItemClickListener itemClickListener) {
-            _listeners.add(itemClickListener);
+            dropdownMenu.listeners.add(itemClickListener);
             return Builder.this;
         }
 
-
-        public Builder addItem(final String itemName, @ColorRes final int itemColor, @DrawableRes final int itemDrawable) {
+        public Builder addItem(@NonNull final String itemName, @ColorRes final int itemColor, @DrawableRes final int itemDrawable) {
 
             //TODO inflate menu item
+
+            View menuItem = dropdownMenu.activity.getLayoutInflater().inflate(R.layout.item_dropdow_menu, null);
+            menuItem.setBackgroundResource(itemColor);
+            ((TextView) menuItem.findViewById(R.id.item_name)).setText(itemName);
+            ((ImageView) menuItem.findViewById(R.id.item_drawable)).setImageResource(itemDrawable);
+            menuItem.setOnClickListener(v -> {
+                for (ItemClickListener cl : dropdownMenu.listeners) {
+                    cl.onItemClicked(((TextView) menuItem.findViewById(R.id.item_name)).getText().toString());
+                }
+            });
+
+            dropdownMenu.rootView.addView(menuItem);
+
+            dropdownMenu.menuItems.add(menuItem);
 
             return Builder.this;
         }
 
         public DropdownMenu build() {
-            if (_activity == null) {
-                throw new RuntimeException("Pass an activity");
+            if (dropdownMenu.menuItems.size() < 1) {
+                throw new RuntimeException("Add at least one items to menu");
             }
-            if (_menuItems.size() < 1) {
-                throw new RuntimeException("Add at least 2 items to menu");
+
+            setMenuViewsParams();
+
+            new Handler().postDelayed(() -> dropdownMenu.hideMenu(), 100);
+            return dropdownMenu;
+        }
+
+        private void setMenuViewsParams() {
+            int n = dropdownMenu.menuItems.size();
+            int screenHeight = screenHeight();
+            int itemHeight = screenHeight / n;
+
+            int multiplier = n;
+
+            ViewGroup.LayoutParams parentPar;
+            ViewGroup.LayoutParams childPar;
+            ViewGroup parent;
+            View child;
+            for (int i = 0; i < n; i++) {
+                parent = (ViewGroup) dropdownMenu.menuItems.get(i);
+                parentPar = parent.getLayoutParams();
+                if (i == 0) {
+                    //it needs to avoid space between last menu element and screen bottom
+                    parentPar.height = screenHeight;
+                } else {
+                    parentPar.height = itemHeight * (--multiplier);
+                }
+                parent.setLayoutParams(parentPar);
+
+                child = parent.getChildAt(0);
+                childPar = child.getLayoutParams();
+                childPar.height = itemHeight;
+                child.setLayoutParams(childPar);
+
+                child = parent.getChildAt(1);
+                childPar = child.getLayoutParams();
+                childPar.height = itemHeight;
+                child.setLayoutParams(childPar);
             }
-            return new DropdownMenu(_menuItems, _listeners, _activity, _rootView);
+
+        }
+
+        private int screenHeight() {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            dropdownMenu.activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            return displayMetrics.heightPixels - getStatusBarHeight();
+        }
+
+        private int getStatusBarHeight() {
+            int result = 0;
+            int resourceId = dropdownMenu.activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = dropdownMenu.activity.getResources().getDimensionPixelSize(resourceId);
+            }
+            return result;
         }
     }
 
+
+    public interface ItemClickListener {
+        void onItemClicked(String itemName);
+    }
+
+
 }
 
-interface ItemClickListener {
-    void onItemClicked (String itemName);
-}
 
